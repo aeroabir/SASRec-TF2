71e2f52d29f5a6fb844a7ffb57a13dcda46fa04d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 13:30:42 +0000

    Adding tarfile member sanitization to extractall()

--- codes_tf1/instacart_preprocess.py
+++ codes_tf1/instacart_preprocess.py
@@ -21,7 +21,26 @@ def initialize_data(tarfile_path, directory, filename, use_columns):
 def unpack_tar_gz(tarfile_path, directory):
 
     with tarfile.open(tarfile_path, 'r:gz') as tf:
-        tf.extractall(path=directory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, path=directory)
 
 
 def make_data(directory, use_columns):
